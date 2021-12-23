2395ff54ef305fed03ad83a6446b3bd994cfe747
augustdan <skop05@mail.ru>
2021-12-23 14:14:07 +0500

    Пофиксил анимацию

--- 2D-Game/Assets/Scripts/CharacterAnim.cs
+++ 2D-Game/Assets/Scripts/CharacterAnim.cs
@@ -1,30 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class CharacterAnim : MonoBehaviour
 {
+    PhotonView view;
     private Animator anim;
     void Start()
     {
+        view = GetComponent<PhotonView>();
         anim = GetComponent<Animator>();
     }
 
-    
+
     void Update()
     {
-        if ((Input.GetAxis("Horizontal")) != 0)
-        {
-            anim.SetBool("isRunning", true);
-        }
-        else
-        {
-            anim.SetBool("isRunning", false);
-        }
+        if (view.IsMine) {
+            if ((Input.GetAxis("Horizontal")) != 0)
+            {
+                anim.SetBool("isRunning", true);
+            }
+            else
+            {
+                anim.SetBool("isRunning", false);
+            }
 
-        if (Input.GetButtonDown("Jump"))
-        {
-            anim.SetTrigger("Jump");
-        }
+            if (Input.GetButtonDown("Jump"))
+            {
+                anim.SetTrigger("Jump");
+            }
+        } 
     }
 }
