3c89c70db1ba949c6a550cd0116e1aec323c79d1
augustdan <skop05@mail.ru>
2021-12-27 13:02:04 +0500

    Разделил анимацию атаки

--- 2D-Game/Assets/Scripts/PlayerController.cs
+++ 2D-Game/Assets/Scripts/PlayerController.cs
@@ -63,6 +63,7 @@ public class PlayerController : MonoBehaviour
             {
                 DashAbility();
                 anim.SetTrigger("DashTrigger");
+                
             } 
         }
     }
