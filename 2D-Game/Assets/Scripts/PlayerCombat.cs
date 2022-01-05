b3aa235617deab9634b99a76afc3f161c5455608
augustdan <skop05@mail.ru>
2022-01-06 00:47:56 +0300

    Добавил комбо удары
    Сделал по видосу комбо удары. Так что там можно будет сделать не только 2, а ещё больше ударов, если надо.  Ещё нужно выравнять ивенты в анимациях, чтобы комбо прожимались плавно и без задержки.

--- 2D-Game/Assets/Scripts/PlayerCombat.cs
+++ 2D-Game/Assets/Scripts/PlayerCombat.cs
@@ -10,17 +10,26 @@ public class PlayerCombat : MonoBehaviour
     public LayerMask enemyLayer;
 
     public float attackRange = 0.5f;
-    public int attackDamage = 40;     
+    public int attackDamage = 40;
+
+    [Header("Combo punch")]
+    public int combo;
+    public bool attackDo;
+    
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            Attack();
-        }
+        Combo();
+       //if (Input.GetKeyDown(KeyCode.F))
+       // {
+       //     Attack();
+       // }
     }
     private void Attack()
     {
-        anim.SetTrigger("Attack");
 
         Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayer);
         foreach(Collider2D enemy in hitEnemies)
@@ -33,5 +42,27 @@ public class PlayerCombat : MonoBehaviour
         if (attackPoint == null)
             return;
         Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+    public void Combo()
+    {
+        if (Input.GetKeyDown(KeyCode.F) && !attackDo)
+        {
+            attackDo = true;
+            anim.SetTrigger("" + combo);
+            Attack();
+        }
+    }
+    public void Start_Combo()
+    {
+        attackDo = false;
+        if( combo < 2)
+        {
+            combo++;
+        }
+    }
+    public void Finish_Animation()
+    {
+        attackDo = false;
+        combo = 0;
     }
 }
